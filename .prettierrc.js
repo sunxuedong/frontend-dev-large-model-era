3540c8df3fd5eb01eca06bea22ced0a5639f6d2d
孙雪冬 <hal_focus@163.com>
2025-10-14 16:59:09 +0800

    feat: deepseek api 调用

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: "avoid",
+  trailingComma: "es5",
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: false,
+  bracketSameLine: false,
+  tabWidth: 4,
+};
--- deepseek_basic/script.js
+++ deepseek_basic/script.js
@@ -5,11 +5,22 @@ const headers = {
 };
 
 const payload = {
+    /**
+     * model 字段指定了要调用的模型类型，Deepseek Platform 支持两个模型。其中 deepseek-chat 是基础模型，目前版本是 v3，deepseek-reasoner 是深度思考模型，目前版本号是 r1。
+     */
     model: 'deepseek-chat',
+    /**
+     * messages 字段指定了对话历史，对话历史是一个数组，数组中的每个元素都是一个对象，对象包含 role 和 content 两个字段。
+     * role 字段指定了对话角色，目前支持 system、user、assistant、function、tool 四个角色。
+     * content 字段指定了对话内容，内容可以是字符串或数组。
+     */
     messages: [
         {role: "system", content: "You are a helpful assistant."},
         {role: "user", content: "你好 Deepseek"}
     ],
+    /**
+     * stream 字段指定了是否启用流式返回，如果设置为 true，则返回的数据会是流式数据，否则返回的数据是完整的 JSON 数据。
+     */
     stream: false,
 };
 
