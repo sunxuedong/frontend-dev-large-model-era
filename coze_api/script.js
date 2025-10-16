e4db474ffb7abe6425e3fbba5851a791e66910cb
孙雪冬 <hal_focus@163.com>
2025-10-16 15:55:03 +0800

    coze api 添加注释

--- coze_api/script.js
+++ coze_api/script.js
@@ -1,5 +1,13 @@
 const endpoint = 'https://api.coze.cn/open_api/v2/chat';
 
+/**
+ * Coze 的 API 调用需要传 bot_id 和 user 参数，
+ * 其中 bot_id 就是我们创建的通用智能体的 ID，前面我们说过它可以从浏览器地址栏的 URL 中获得
+ * user 可以是一个任意字符串，只是用来标识。
+ * 此外，Coze API 的 query 和 chat_history 是分开的，当前输入内容以 query 字段传入，格式是一个字符串，而历史消息以 chat_history 字段传入，格式和前面 Deepseek 的 messages 差不多，具体可以看 Coze 的官方文档。
+ * 如果不需要历史消息，只需要传入一个空的数组。
+ * 由于 Coze 没有 system 消息，它的提示词是在人设与回复逻辑中设置，我们在前面创建时，已经定义了一个叫 prompt 的模板变量，所以在这里我们可以通过 custom_variables 参数将 prompt 变量的具体值传入
+ */
 const payload = {
     bot_id: import.meta.env.VITE_BOT_ID,
     user: 'yvo',
@@ -7,8 +15,8 @@ const payload = {
     chat_history: [],
     stram: false,
     custom_variables: {
-        prompt: "你是一个AI助手"
-    }
+        prompt: '你是一个AI助手',
+    },
 };
 
 const response = await fetch(endpoint, {
